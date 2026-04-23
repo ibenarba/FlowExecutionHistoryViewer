1b1c249e51ed05580a02721bab996e99e46eb8e5
Ibtissam Benarba <ibenarba@smb.sqli.com>
2026-04-23 16:31:11 +0200

    Initial commit

---
+++ FlowExecutionHistoryViewer/MyPluginControl.cs
@@ -0,0 +1,213 @@
+﻿using FlowExecutionHistoryViewer.Models;
+using FlowExecutionHistoryViewer.Services;
+using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Identity.Client;
+namespace FlowExecutionHistoryViewer
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        private void tsbSample_Click(object sender, EventArgs e)
+        {
+            // The ExecuteMethod method handles connecting to an
+            // organization if XrmToolBox is not yet connected
+            ExecuteMethod(GetAccounts);
+        }
+
+        private void GetAccounts()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting accounts",
+                Work = (worker, args) =>
+                {
+                    args.Result = Service.RetrieveMultiple(new QueryExpression("account")
+                    {
+                        TopCount = 50
+                    });
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        MessageBox.Show($"Found {result.Entities.Count} accounts");
+                    }
+                }
+            });
+        }
+
+        private void btnFetchHistory_Click(object sender, EventArgs e)
+        {
+            if (ConnectionDetail == null) return;
+
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Connexion à l'API Flow France...",
+                Work = (worker, args) =>
+                {
+                    var envId = ConnectionDetail.EnvironmentId.ToString();
+                    var scopes = new[] { "https://service.flow.microsoft.com/.default" };
+
+                    // On récupère le token
+                    var pca = PublicClientApplicationBuilder.Create("51f81489-12ee-4a9e-aaae-a2591f45987d")
+                        .WithAuthority($"https://login.microsoftonline.com/{ConnectionDetail.TenantId}")
+                        .WithRedirectUri("app://58145B91-0C36-4500-8554-080854F2AC97")
+                        .Build();
+
+                    string token = GetAccessToken(pca, scopes);
+
+                    // URL spécifique pour crm12 (France)
+                    string regionalUrl = "https://france.api.flow.microsoft.com";
+
+                    var client = new FlowClient(envId, token, regionalUrl);
+
+                    // Testez avec l'ID de flux que vous avez fourni
+                    args.Result = client.GetFlowRuns("304d85b8-ee0c-c6ff-88ff-114773a67695");
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        // Affiche l'erreur complète pour voir s'il s'agit d'un problème DNS ou SSL
+                        MessageBox.Show($"Détails de l'erreur : {args.Error.InnerException?.Message ?? args.Error.Message}");
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = (List<FlowRun>)args.Result;
+                    }
+                }
+            });
+        }
+
+        private string GetAccessToken(IPublicClientApplication pca, string[] scopes)
+        {
+            try
+            {
+                var accounts = pca.GetAccountsAsync().GetAwaiter().GetResult();
+                var account = accounts.FirstOrDefault(a => a.Username.Equals(ConnectionDetail.UserName, StringComparison.OrdinalIgnoreCase));
+
+                if (account != null)
+                {
+                    return pca.AcquireTokenSilent(scopes, account).ExecuteAsync().GetAwaiter().GetResult().AccessToken;
+                }
+            }
+            catch (MsalUiRequiredException) { /* On continue vers l'interactif */ }
+
+            // Pour l'interactif, on utilise une petite astuce pour ne pas bloquer le thread UI
+            string interactiveToken = null;
+            var task = Task.Run(async () =>
+            {
+                var result = await pca.AcquireTokenInteractive(scopes)
+                    .WithLoginHint(ConnectionDetail.UserName)
+                    .ExecuteAsync();
+                interactiveToken = result.AccessToken;
+            });
+
+            task.Wait(); // On attend la fin de la tâche asynchrone
+            return interactiveToken;
+        }
+
+        // Méthode d'aide pour gérer l'affichage de la fenêtre sur le bon thread
+        private string ShowLoginWindow(IPublicClientApplication pca, string[] scopes)
+        {
+            string token = null;
+            this.Invoke(new MethodInvoker(delegate
+            {
+                var result = pca.AcquireTokenInteractive(scopes)
+                    .WithLoginHint(ConnectionDetail.UserName)
+                    .WithParentActivityOrWindow(this.Handle)
+                    .ExecuteAsync().GetAwaiter().GetResult();
+                token = result.AccessToken;
+            }));
+            return token;
+        }
+
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnFetchHistory_Click(sender, e);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FlowExecutionHistoryViewer/MyPluginControl.designer.cs
@@ -0,0 +1,136 @@
+﻿namespace FlowExecutionHistoryViewer
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSample = new System.Windows.Forms.ToolStripButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadFlows = new System.Windows.Forms.Button();
+            this.cmbFlows = new System.Windows.Forms.ComboBox();
+            this.toolStripMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1,
+            this.tsbSample});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(746, 31);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(107, 28);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // tsbSample
+            // 
+            this.tsbSample.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbSample.Name = "tsbSample";
+            this.tsbSample.Size = new System.Drawing.Size(57, 28);
+            this.tsbSample.Text = "Try me";
+            this.tsbSample.Click += new System.EventHandler(this.tsbSample_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(130, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(613, 337);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnLoadFlows
+            // 
+            this.btnLoadFlows.Location = new System.Drawing.Point(3, 224);
+            this.btnLoadFlows.Name = "btnLoadFlows";
+            this.btnLoadFlows.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFlows.TabIndex = 6;
+            this.btnLoadFlows.Text = "btnLoadFlows";
+            this.btnLoadFlows.UseVisualStyleBackColor = true;
+            this.btnLoadFlows.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbFlows
+            // 
+            this.cmbFlows.FormattingEnabled = true;
+            this.cmbFlows.Location = new System.Drawing.Point(3, 100);
+            this.cmbFlows.Name = "cmbFlows";
+            this.cmbFlows.Size = new System.Drawing.Size(121, 24);
+            this.cmbFlows.TabIndex = 7;
+            this.cmbFlows.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmbFlows);
+            this.Controls.Add(this.btnLoadFlows);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(746, 370);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadFlows;
+        private System.Windows.Forms.ComboBox cmbFlows;
+    }
+}
